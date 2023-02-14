effa8118b510da5640abf16190fafd3eb457ebf5
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-02-14 20:05:08 +0300

    chore:get single product

--- controllers/getSingleProduct.js
+++ controllers/getSingleProduct.js
@@ -1,9 +1,18 @@
+const mongoose =  require('mongoose')
 const ProductSchema = require("../models/ProductSchema");
+
 const getSingleProduct=async(req,res,next)=>{
-    const {productId}=req.params
+    const {productId }=req.params
     try {
-        const product=await ProductSchema.findOne({ "_id":productId})
+        if(mongoose.Types.ObjectId.isValid(productId)){
+        const product = await ProductSchema.findOne({ "_id":productId});
+
        res.status(200).send({"data": product})
+        }
+
+        else{
+            res.status(404).send({"message":"product not found"})
+        }
     } catch (error) {
         next(error)
     }
--- routes/api.route.js
+++ routes/api.route.js
@@ -1,11 +1,15 @@
 const addProducts = require('../controllers/AddProduct');
 const getAllProducts = require('../controllers/getAllProduct');
-
+const getSingleProduct = require('../controllers/getSingleProduct');
 const router = require('express').Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (_req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 router.post('/addProduct',addProducts)
-router.get('/products',getAllProducts);
+router.get('/products/:dataAmount',getAllProducts)
+router.route('/product/:productId').get(getSingleProduct)
+
+
+
 module.exports = router;
