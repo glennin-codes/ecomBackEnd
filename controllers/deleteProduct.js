92b34d039c27e2990d3c88b18a9e35d3b571e692
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-02-17 12:05:10 +0300

    chore:add some staffs

--- controllers/deleteProduct.js
+++ controllers/deleteProduct.js
@@ -4,12 +4,14 @@ const cloudinary = require('cloudinary').v2;
 const deleteProduct = async (req, res, next) => {
   try {
     // Retrieve the product from the database
+
     const product = await ProductSchema.findOne({ _id: req.params.productId });
 if (!product) return res.status(404).send({msg:"item with the given id not found!"})
     // Retrieve the product_id from the image array
     const productIds = product.image.map(image => image.product_id);
 
     // Delete the images from Cloudinary
+    //you can also choose to delete one using cloudinary.upload.destroy(publicID)
     await cloudinary.api.delete_resources(productIds);
       await cloudinary.api.delete_resources(productIds)
 
