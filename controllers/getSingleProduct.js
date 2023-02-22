dc77adc4eb63cc9e4f7f0ae458e068c75fac58d6
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-02-22 11:42:38 +0300

    fixing bug issue

--- controllers/getSingleProduct.js
+++ controllers/getSingleProduct.js
@@ -2,17 +2,27 @@ const mongoose =  require('mongoose')
 const ProductSchema = require("../models/ProductSchema");
 
 const getSingleProduct=async(req,res,next)=>{
-    const {productId }=req.params
+    // console.log(req.params)
+
+    const id = req.params.id;
+    // const id = req.query
+    // const id = req.query.id
+    console.log(id);
+
     try {
-        if(mongoose.Types.ObjectId.isValid(productId)){
-        const product = await ProductSchema.findOne({ "_id":productId});
+       
+        const product = await ProductSchema.findOne({ _id:id});
+
+
+        console.log(product)
+
+
+
 
        res.status(200).send({"data": product})
-        }
+     
 
-        else{
-            res.status(404).send({"message":"product not found"})
-        }
+        
     } catch (error) {
         next(error)
     }
