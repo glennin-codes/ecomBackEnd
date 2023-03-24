b5b1813e3494e716671f49ac47365592315f0cc7
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-24 21:24:51 +0300

    nodemailerApi

--- Emails/ContactUS/emailer.js
+++ Emails/ContactUS/emailer.js
@@ -2,7 +2,7 @@ const Emailer= async (req, res) => {
     try {
       const { FirstName,LastName,email,number,message} = req.body
       EmailSender({FirstName,LastName,email,number,message})
-      res.json({ msg: "Your message sent successfully" });
+      res.status(200).json({ msg: "Your message sent successfully" });
     } catch (error) {
       res.status(404).json({ msg: "Error ❌" });
     }
