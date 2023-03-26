f680aa8516e687c90e8d40dc2d9cb90e015fd023
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-03-26 03:25:13 +0300

    nodemailerApi

---
+++ Emails/Contactus/util/Config.js
@@ -0,0 +1,10 @@
+
+const config = {
+  user:'milesmotorssocialmedia@gmail.com' ,
+  clientId:'892413807937-fdod848avrv1lrj1pkio58ll92ht427i.apps.googleusercontent.com',
+  clientSecret:'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',
+  refreshToken:'1//04lQqoy3l01U3CgYIARAAGAQSNwF-L9IrWnLwTLnn0OOH7s0bdav2fiDPjn4fvvnYQaYD-3DOZobIbs73g4UnR6HWKFiKPyqKZY8',
+  clientUrl:'https://developers.google.com/oauthplayground',
+
+};
+module.exports=config
