6be57ffe24abfa49ce2cf92a5c7fbba743a1c0af
GLEN <109092642+glennin-codes@users.noreply.github.com>
2023-03-27 21:24:13 +0300

    Update regester.js

--- controllers/auth/regester.js
+++ controllers/auth/regester.js
@@ -23,6 +23,7 @@ async function registerUser(req, res) {
   try {
     const { error, value } = userSchema.validate(req.body);
     if (error) {
+      console.log(error.details[0].message )
       return res.status(400).json({ error: error.details[0].message });
     }
 
