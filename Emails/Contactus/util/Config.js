63539844e7f5c4f3847a92414ed5c2029d3baa90
ayiendaglen <ayiendaglen@gmail.com>
2023-03-28 06:31:34 +0000

    email config files

--- Emails/Contactus/util/Config.js
+++ Emails/Contactus/util/Config.js
@@ -1,9 +1,9 @@
 
 const config = {
-  user:'milesmotorssocialmedia@gmail.com' ,
+  user:'ayiendaglen@gmail.com' ,
   clientId:'892413807937-fdod848avrv1lrj1pkio58ll92ht427i.apps.googleusercontent.com',
   clientSecret:'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',
-  refreshToken:'1//04lQqoy3l01U3CgYIARAAGAQSNwF-L9IrWnLwTLnn0OOH7s0bdav2fiDPjn4fvvnYQaYD-3DOZobIbs73g4UnR6HWKFiKPyqKZY8',
+  refreshToken:'1//04q_EPlfxiN2hCgYIARAAGAQSNwF-L9IrG2pDh6c35g35uCmsKNATLDikeV4pZMFJof6yjj0lsQ6gbo8mFV_u8tphE1CkZ5joMQY',
   clientUrl:'https://developers.google.com/oauthplayground',
 
 };
