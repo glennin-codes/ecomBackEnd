8068669503a742ea3e595e39e1d302a9324fe58f
ayiendaglen <ayiendaglen@gmail.com>
2023-03-28 11:22:13 +0000

    Updated Config.js

--- Emails/Contactus/util/Config.js
+++ Emails/Contactus/util/Config.js
@@ -4,7 +4,7 @@ const config = {
   clientId:'892413807937-fdod848avrv1lrj1pkio58ll92ht427i.apps.googleusercontent.com',
   clientSecret:'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',
   refreshToken:'1//04ZYo5ZChxI8HCgYIARAAGAQSNwF-L9IrMkBir3wh0hJGLAnbKEVSVNSP5lkFyQz3ixnOIQJ6OGxBJDITMZw8kdqg4ZlbiXlRdgQ',
-  clientUrl:'https://developers.google.com/oauthplayground',
+  clientUrl:'https://comradesbizapi.azurewebsites.net',
 
 };
 module.exports=config
