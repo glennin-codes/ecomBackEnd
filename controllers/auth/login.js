f77a6583d8eef9735005fb967e458df2892c36c9
ayiendaglen <ayiendaglen@gmail.com>
2023-03-29 22:09:43 +0000

    Updated login.js

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -26,7 +26,11 @@ async function loginUser(req, res) {
         { expiresIn: '1h' }
       );
   
-      res.status(200).json({ token });
+      res.status(200).json({ token ,
+       email:user.email,
+    name: user.name,
+    id:user._id
+      });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal server error' });
