36acc4066db3722271ad24899299adddb157a598
ayiendaglen <ayiendaglen@gmail.com>
2023-04-01 21:07:46 +0000

    Updated getSingleUser.js

--- controllers/User/getSingleUser.js
+++ controllers/User/getSingleUser.js
@@ -1,18 +1,22 @@
 const User = require("../../models/user");
 
-// Get single user by ID
+// Get single user by ID or email
 const getSingleUser = async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
+  const idOrEmail = req.params.id;
+  let user;
+
+  // Check if the parameter is a valid email address
+  if (/^\S+@\S+\.\S+$/.test(idOrEmail)) {
+    user = await User.findOne({ email: idOrEmail });
+  } else {
+    user = await User.findById(idOrEmail);
+  }
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
+};
   
-  module.exports = getSingleUser
- 
+module.exports = getSingleUser;
