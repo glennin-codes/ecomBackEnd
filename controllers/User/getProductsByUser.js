a4a18a735e869ef25714b6c3d7ef6d29c2129a12
ayiendaglen <ayiendaglen@gmail.com>
2023-04-03 14:31:53 +0300

    cleaning sub bugs

--- controllers/User/getProductsByUser.js
+++ controllers/User/getProductsByUser.js
@@ -4,7 +4,13 @@ const getProductByUser=async (req,res)=>{
     const email = req.params.email;
    try{
     const data= await ProductSchema.find({ user:email});
+   
+   if (data){
     res.status(200).json(data)
+   }
+    else{
+        res.status(404).json({ message: 'User not found' });
+    }
 
     }catch(error){
        
