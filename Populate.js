78555858ce1f8032794612545c2d492fbc880349
ayiendaglen <ayiendaglen@gmail.com>
2023-04-05 21:49:27 +0300

    changes and added some useful staffs

--- Populate.js
+++ Populate.js
@@ -1,8 +1,17 @@
+const { default: mongoose } = require("mongoose");
 const User = require("./models/user");
-
+const connect = require("./db/config");
+const dotenv=require('dotenv')
+dotenv.config();
 const Delete = async () => {
-  await User.deleteMany();
-  console.log("deleted users ");
+  const Url = process.env.Mongo_Url;
+  try {
+    await connect(Url)
+    await User.deleteMany();
+    console.log("deleted users ");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 Delete();
