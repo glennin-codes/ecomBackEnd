f16be3b2fab1c0276196b8750517aae993d5b13b
ayiendaglen <ayiendaglen@gmail.com>
2023-04-05 21:10:38 +0300

    changes and added some useful staffs

--- Populate.js
+++ Populate.js
@@ -1,5 +1,8 @@
-const delete=()=>{
-.deleteMany();
+const User = require("./models/user");
 
+const Delete = async () => {
+  await User.deleteMany();
+  console.log("deleted users ");
+};
 
-}
+Delete();
