962c8adde308e0ca27a39e08c2b1d674924837b1
ayiendaglen <ayiendaglen@gmail.com>
2023-05-02 01:25:52 +0300

    night

--- controllers/User/DeleteUser.js
+++ controllers/User/DeleteUser.js
@@ -2,39 +2,38 @@ const ProductSchema = require("../../models/ProductSchema");
 const User = require("../../models/user");
 
 // Delete user by ID
-const deleteUser= async (req, res) => {
-    const { id } = req.params;
-  
-    try {
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
 
-
-
-
-      const deletedUser = await User.findOne({_id:id});
-      if (!deletedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      const product = await ProductSchema.findOne({user:deletedUser.email});
-      if (!product) {
-        return res.status(404).send({msg:"item Not Found"})
-      }
-          // Retrieve the product_id from the image array
-          const productIds = await Promise.all(product.image.map(image => image.product_id));
-
-      
-          // Delete the images from Cloudinary
-          //you can also choose to delete one using cloudinary.upload.destroy(publicID)
-          await cloudinary.api.delete_resources(productIds);
-           
-
-      // Delete the user from the database
-     await User.deleteOne({ _id: req.params.id });
-     
-  
-      res.status(200).json({ message: 'User deleted successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+  try {
+    const deletedUser = await User.findOne({ _id: id });
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
-  module.exports =deleteUser;
+    const products = await ProductSchema.find({ user: deletedUser.email });
+    if (products.length === 0) {
+      console.log("No products found for user");
+    } else {
+      // Retrieve the product_ids from the images array of all the products
+      const productIds = products
+        .map((product) => product.image.map((image) => image.product_id))
+        .flat();
+        
+      // Delete the images from Cloudinary
+      await cloudinary.api.delete_resources(productIds);
+      
+      // Delete all the products that are linked to the deleted user
+      await ProductSchema.deleteMany({ user: deletedUser.email });
+    }
+
+    // Delete the user from the database
+    await User.deleteOne({ _id: id });
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = deleteUser;
